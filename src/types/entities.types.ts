fa4b133dcc9940c81bef4993439947660c5f23f7
Daniel Gibbons <daniel@airmanual.co>
2025-03-09 20:31:28 -0300

    add new entity types

---
+++ src/types/entities.types.ts
@@ -0,0 +1,34 @@
+export interface Planet {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  residents: string[]; // URLs
+  films: string[]; // URLs
+  created: string;
+  edited: string;
+  url: string; // URL
+}
+
+export interface Species {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: string;
+  skin_colors: string;
+  hair_colors: string;
+  eye_colors: string;
+  average_lifespan: string;
+  homeworld: Planet | null;
+  language: string;
+  people: string[]; // URLs
+  films: string[]; // URLs
+  created: string;
+  edited: string;
+  url: string; // URL
+}
